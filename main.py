de792048bbbcad31b70d8cac698317fc7df4b9db
kirsty-evans <kirstysevans@gmail.com>
2025-01-16 22:09:10 +1100

    wrote the whole program

---
+++ main.py
@@ -0,0 +1,35 @@
+def count_words(text):
+    words = text.split()
+    print(f"{len(words)} words found in the document")
+
+def sort_key(dict):
+    return dict["count"]
+
+def count_char(text):
+    lowercase = text.lower()
+    dictionary = {}
+    for character in lowercase:
+        if character.isalpha():
+            if character not in dictionary:
+                dictionary[character] = 1
+            else:
+                dictionary[character] += 1
+    converted_list = []
+    for item in dictionary:
+        newdict = {"letter":item, "count": dictionary[item]}
+        converted_list.append(newdict)
+    converted_list.sort(reverse=True, key = sort_key)
+
+    for item in converted_list:
+            print(f"The '{item["letter"]}' character was found {item["count"]} times")
+
+
+def main():
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read()
+    print(f"--- Begin report of books/frankenstein.txt ---")
+    count_words(file_contents)
+    count_char(file_contents)
+    print("--- End report ---")
+
+main()
